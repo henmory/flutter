e54ed6193c22d5888036e29e26675008bac1e420
henmory <hanmh87@163.com>
2019-03-07 14:36:06 +0800

    1.new commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,91 +5,77 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget title = Container(
-      padding: EdgeInsets.all(32.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(bottom: 12.0),
-                  child: Text(
-                    "Oeschinen Lake Campground",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  "Kandersteg, Switzerland",
-                    style: TextStyle(color: Colors.grey),
-                ),
-              ],
-            ),
+    Widget rating = Container(
+      padding: EdgeInsets.all(5.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Icon(
+            Icons.star,
+            color: Colors.black,
           ),
           Icon(
             Icons.star,
-            color: Colors.red,
+            color: Colors.black,
           ),
-          Container(
-            padding: EdgeInsets.all(5.0),
-            child: Text("41"),
+          Icon(
+            Icons.star,
+            color: Colors.black,
           ),
-        ],
-      ),
+          Icon(
+            Icons.star,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.black,
+          ),
+        ]),
+        Text(
+          '170 Reviews',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+          ),
+        ),
+      ]),
     );
-    Widget button = Container(
-      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+    Widget item(IconData icon, description, time) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Icon(icon, color: Colors.green,),
+            Container(
+              padding: EdgeInsets.only(top: 5.0),
+              child: Text(description),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 5.0),
+              child: Text(time),
+            ),
+          ]);
+    }
+
+    Widget details = Container(
+      padding: EdgeInsets.all(2.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          item(Icons.call, "CALL"),
-          item(Icons.near_me, "ROUTER"),
-          item(Icons.share, "SHARE"),
+        children: <Widget>[
+          item(Icons.kitchen, "PREP", "25 min"),
+          item(Icons.timer, "COOK", "1 hr"),
+          item(Icons.restaurant, "FEEDS", "4-6"),
         ],
       ),
     );
-    Widget text = Container(
-        padding: EdgeInsets.only(left: 32.0, right: 32.0),
-        child:Text(
-          '''Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes.A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.''',
-        ),
-    );
+
     return MaterialApp(
-      title: "demo",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Demo"),
+          title: Text('Demo'),
         ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/photo.jpeg',
-              fit: BoxFit.cover,
-            ),
-            title,
-            button,
-            text,
-          ],
+        body: Column(
+          children: <Widget>[rating, details],
         ),
       ),
-    );
-  }
-
-  Widget item(IconData icon, String data) {
-    Color color = Colors.blue;
-    return Column(
-
-      children: [
-        Icon(
-          icon,
-          color: color,
-        ),
-        Text(
-          data,
-          style: TextStyle(color: color),
-        ),
-      ],
     );
   }
 }
