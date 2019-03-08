4560c6c6cb54a877cd804f61c88e105bb5122fab
henmory <hanmh87@163.com>
2019-03-08 15:33:11 +0800

    1.add new

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,63 @@
-import 'package:flutter/material.dart';
+void main() {
+  Future.wait([
+    Future.delayed(new Duration(seconds: 2), () {
+      return "hello";
+    }),
+    Future.delayed(new Duration(seconds: 2), () {
+      return "world";
+    })
 
-void main() => runApp(MyApp());
+  ]).then((data) {
+    print(data);
+  }, onError: (data) => print(data));
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Widget rating = Container(
-      padding: EdgeInsets.all(5.0),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          Icon(
-            Icons.star,
-            color: Colors.black,
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.black,
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.black,
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.black,
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.black,
-          ),
-        ]),
-        Text(
-          '170 Reviews',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20.0,
-          ),
-        ),
-      ]),
-    );
-    Widget item(IconData icon, description, time) {
-      return Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Icon(icon, color: Colors.green,),
-            Container(
-              padding: EdgeInsets.only(top: 5.0),
-              child: Text(description),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 5.0),
-              child: Text(time),
-            ),
-          ]);
+  Future<String> login(String usrName, String passWord) {
+    return Future<String>(() {
+      return "fadfa";
+    });
+  }
+
+  Future<String> getUsrInformation(String id) {
+    return Future<String>(() {
+      return "infroation";
+    });
+  }
+
+  Future<bool> saveInformation(String information) {
+    return Future<bool>(() {
+      print("ok");
+    });
+  }
+
+
+  login("afda", "afda").then((id) {
+    getUsrInformation(id).then((information) {
+      saveInformation(information).then((data){
+        print(data);
+      });
+    });
+  });
+
+  login("daf","daafa").then((id){
+    return getUsrInformation(id);
+  }).then((information){
+    return saveInformation(information);
+  }).then((ret){
+    print(ret);
+  }).catchError((onError){
+    print(onError);
+  });
+
+
+  task() async{
+    try{
+      String id = await login("af", "afa");
+      String information = await getUsrInformation(id);
+      bool ret = await saveInformation(information);
+
+    }catch(e){
+      print(e);
     }
-
-    Widget details = Container(
-      padding: EdgeInsets.all(2.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          item(Icons.kitchen, "PREP", "25 min"),
-          item(Icons.timer, "COOK", "1 hr"),
-          item(Icons.restaurant, "FEEDS", "4-6"),
-        ],
-      ),
-    );
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Demo'),
-        ),
-        body: Column(
-          children: <Widget>[rating, details],
-        ),
-      ),
-    );
   }
 }
+
